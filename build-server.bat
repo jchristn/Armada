c393a008942187220c3561900fc94b60fb47b6c0
Joel Christner <joel.christner@gmail.com>
2026-03-12 13:44:16 -0700

    feat: add build-server.bat and build-server.sh build scripts
    Add cross-platform build scripts for building and pushing the
    Armada server Docker image for linux/amd64 and linux/arm64/v8.
    
    Armada-Mission-Id: msn_mmnxqx4h_mC3UcqRBn06
    Armada-Voyage-Id: vyg_mmnxqvcu_0wLBAm14kZy
    Armada-Captain-Id: cpt_mmiwuvj2_bUC235KmLEe
    Armada-Vessel-Id: vsl_mmit8chk_PTvi4HamUKb

---
+++ build-server.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if "%~1"=="" (
+    echo Usage: build-server.bat ^<version-tag^>
+    echo Example: build-server.bat v0.1.0
+    exit /b 1
+)
+
+docker buildx build ^
+    --platform linux/amd64,linux/arm64/v8 ^
+    -f src/Armada.Server/Dockerfile ^
+    -t jchristn77/armada-server:latest ^
+    -t jchristn77/armada-server:%~1 ^
+    --push ^
+    .
